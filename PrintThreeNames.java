7d3dc5d8dbda0ce8956fbab22ecfd8378f02f46d
Chaithra <chaithrashenoy08@gmail.com>
2018-05-16 11:04:15 +0530

    First commit

---
+++ PrintThreeNames.java
@@ -0,0 +1,15 @@
+/*Write a program “ PrintThreeNames.java ” that takes three names as input and
+prints out a proper sentence with the names in the reverse of the order given, so
+that for example, " java PrintThreeNames Alice Bob Carol " gives "Hi Carol, Bob,
+and Alice.".*/
+public class PrintThreeNames
+{
+public static void main(String[] args)
+{
+String a="Alice";
+String b="Bob";
+String c="Carol";
+System.out.print("Hi"+" "+c+", "+b+", "+a);
+System.out.println();
+}
+}
